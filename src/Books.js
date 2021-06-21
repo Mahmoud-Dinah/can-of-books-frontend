7658caf5b9eb43a6778bc27642712d9b15db649b
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-06-21 22:11:36 +0300

    rendering favorite books

--- src/Books.js
+++ src/Books.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 class Books extends React.Component {
   render() {
     return (
       <>
-        <h2>My Favorite Books</h2>
+        <h2 style={{ textAlign: 'center' }}>My Favorite Books</h2>
         {this.props.books.length &&
           this.props.books.map((book, idx) => (
-            <div key={idx}>
-              {book.name}
-              {book.description}
-              {book.status}
+            <div key={idx} style={{ textAlign: 'center', color: 'pa-primary' }}>
+              <ListGroup>
+                <ListGroup.Item>{book.name}</ListGroup.Item>
+                <ListGroup.Item>{book.description}</ListGroup.Item>
+                <ListGroup.Item>{book.status}</ListGroup.Item>
+                <br></br>
+              </ListGroup>
             </div>
           ))}
       </>
